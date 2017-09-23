c7e3c0172a80d444636aa284b1531ba8d8eb884f
Lydia Krasucki <lydiakrasucki@Lydias-MacBook-Pro.local>
2017-09-22 19:35:33 -0500

    First commit

--- Person UI/ViewController.swift
+++ Person UI/ViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBAction func slider(_ sender: UISlider)
+    {
+        label1.text = String(Int(sender.value))
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+        
     }
 
 
